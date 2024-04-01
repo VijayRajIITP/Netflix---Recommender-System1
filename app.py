56bde0c206cafb1dbf235d7a103950093d5bc6a8
VijayRajIITP <149241319+VijayRajIITP@users.noreply.github.com>
2024-04-02 00:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,6 +152,5 @@ def recommend():
     return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
         vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
         movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
